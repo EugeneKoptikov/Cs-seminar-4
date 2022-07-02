bc4f4f2a6f7b760f03f0ba2860934f7dc0443b20
EugenKoptikov <evgenkoptikov100500@gmail.com>
2022-07-03 00:40:11 +0300

    Exercise 27 created!

---
+++ Exercise 27/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.WriteLine("Программа, принимает число и выдает сумму цифр из числа");
+Console.Write("Введите число: ");
+string number = Console.ReadLine();
+Console.WriteLine(" ");
+
+int index = 0;
+char symbol;
+int element;
+int sum = 0;
+
+while (index < number.Length)
+{
+    symbol = number[index];
+    element = (int)Char.GetNumericValue(symbol);
+    sum = sum + element;
+    index++;
+ 
+}
+
+Console.WriteLine("Сумма цифр: " + sum);
