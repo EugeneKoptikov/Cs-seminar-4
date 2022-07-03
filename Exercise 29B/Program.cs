2219ec708581c63c66790e7b5bf7b47790550a53
EugenKoptikov <evgenkoptikov100500@gmail.com>
2022-07-03 17:59:18 +0300

    Another solution!

---
+++ Exercise 29B/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// Вариант решения с вводом размера массива.
+Console.WriteLine("Программа создает массив на указанное число элементов со случайными значениями (от 1 до 99).");
+Console.Write("Укажите размер массива: ");
+Console.WriteLine(" ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[number];
+
+void CreatArray(int[] collection)
+{
+    int index = 0;
+
+    while (index < number)
+    {
+        array[index] = new Random().Next(1, 99);
+        index++;
+    } 
+}
+
+void PrintArray(int[] arrayPrint)
+{
+    int indexPrint = 0;
+
+    while (indexPrint < number)
+    {
+        if (indexPrint == 0)
+        {
+            Console.Write("[");
+        }
+
+        Console.Write(arrayPrint[indexPrint]);
+
+        if (indexPrint < number - 1) 
+        {
+            Console.Write(", ");
+        }
+
+        if (indexPrint == number - 1)
+        {
+            Console.Write("]");
+        }
+        indexPrint++;
+    }
+}
+
+CreatArray(array);
+PrintArray(array);
