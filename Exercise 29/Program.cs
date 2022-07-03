4854e1b8866b565309de17d8ee3b158198adb1ec
EugenKoptikov <evgenkoptikov100500@gmail.com>
2022-07-03 17:44:26 +0300

    Exercise 29 created!

---
+++ Exercise 29/Program.cs
@@ -0,0 +1,44 @@
+﻿
+Console.WriteLine("Программа создает массив на восемь элементов со случайными значениями (от 1 до 99).");
+
+int[] array = new int[8];
+
+void CreatArray(int[] collection)
+{
+    int lenth = 8;  // В условии задачи указана 8 как размер массива, но у меня не срабатывало определение "collection.Lenth".
+    int index = 0;
+
+    while (index < lenth)
+    {
+        array[index] = new Random().Next(1, 99);
+        index++;
+    } 
+}
+
+void PrintArray(int[] arrayPrint)
+{
+    int lenthPrint = 8;     // "arrayPrint.Lenth" здесь тоже не работает.
+    int indexPrint = 0;
+    while (indexPrint < lenthPrint)
+    {
+        if (indexPrint == 0)
+        {
+            Console.Write("[");
+        }
+
+        Console.Write(arrayPrint[indexPrint]);
+        if (indexPrint < lenthPrint - 1) 
+        {
+            Console.Write(", ");
+        }
+
+        if (indexPrint == lenthPrint - 1)
+        {
+            Console.Write("]");
+        }
+        indexPrint++;
+    }
+}
+
+CreatArray(array);
+PrintArray(array);
